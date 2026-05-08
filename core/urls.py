54e1b234f7795b446bada2584e341c4135ad77e9
VICTOR <vt74117@gmail.com>
2026-05-08 16:18:28 -0500

    Agregar configuración de plantillas y nuevas rutas para la app de usuarios

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <-- Importa 'include'
+
+# core/urls.py
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Conectamos las urls de la app usuarios a la ruta '/usuarios/'
+    path('usuarios/', include('usuarios.urls')), 
+    
 ]
