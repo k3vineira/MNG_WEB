e7fdff23e7e91c4c4b2699bb0585cde2d814d833
Kevin Neira <neirak424@gmail.com>
2026-05-10 22:24:51 -0500

    - refactor: Plantilla de cabecera actualizada para implementar un menú superpuesto a pantalla completa. - refactor: Eliminaron las plantillas antiguas de registro y términos, reemplazándolas por nuevas plantillas estructuradas. -feat:  Se añadieron la vista de inicio de sesión y se actualizaron las URLs para la autenticación de usuarios. - style:  Creación de nuevas plantillas de inicio de sesión, registro y términos con estilo y estructura mejorados. - feat: Implementó la validación de formularios para los procesos de registro e inicio de sesión.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,57 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render
 
+@login_required
 def inicio_usuarios(request):
-   
     context = {
-        'titulo': 'Panel de Usuarios'
+        'titulo': 'Panel de Usuarios',
+        'usuario': request.user
     }
-    
     return render(request, 'index-admin.html', context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('usuarios:inicio')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(
+                username=username,
+                password=password
+            )
+            if user is not None:
+                auth_login(request, user)
+                messages.success(
+                    request,
+                    f"¡Bienvenido de nuevo, {user.username}!"
+                )
+                return redirect('usuarios:inicio')
+    else:
+        form = AuthenticationForm()
+    return render(
+        request,
+        'authentication/login.html',
+        {'form': form}
+    )
+
+
+def registro_view(request):
+    return render(
+        request,
+        'authentication/registro.html'
+    )
+
+
+def terminos_view(request):
+    return render(
+        request,
+        'authentication/terminos.html'
+    )
