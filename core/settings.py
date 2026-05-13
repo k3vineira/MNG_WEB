5125dca800ef0ad0f0bc90661c28a8832a3c6dad
Kevin Neira <neirak424@gmail.com>
2026-05-12 20:37:58 -0500

    feat: implementar flujo de restablecimiento de contraseña con vistas y plantillas personalizadas

--- core/settings.py
+++ core/settings.py
@@ -130,3 +130,13 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/usuarios/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'neirak424@gmail.com' 
+EMAIL_HOST_PASSWORD = 'qsxg jzbi mtly ppjf'
+
+DEFAULT_FROM_EMAIL = 'Monagua <monagua2026@gmail.com>'
