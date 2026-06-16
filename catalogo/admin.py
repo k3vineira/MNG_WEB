7a91e970124a1b204b09cb36bf7dd9d1f49af043
Kevin Neira <neirak424@gmail.com>
2026-06-16 11:26:05 -0500

    feat: agregar admin

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Temporada, Categoria, Actividades, Paquete, Tarifa, PaqueteActividad
 
 # Register your models here.
+admin.site.register(Temporada)
+admin.site.register(Categoria)
+admin.site.register(Actividades)
+admin.site.register(Paquete)
+admin.site.register(Tarifa)
+admin.site.register(PaqueteActividad)
