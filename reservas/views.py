de44ea4d2728e3a66f6eddfa5ee2b5d2c9526516
daca-btl <dayanamozo2116@gmail.com>
2026-06-17 11:11:19 -0500

    feat: integrar soporte para DataTables en múltiples vistas y mejorar estilos

--- reservas/views.py
+++ reservas/views.py
@@ -312,11 +312,10 @@ def administrar_cancelaciones(request):
         except (InvalidOperation, ValueError, TypeError):
             c.penalidad = Decimal('0.00')
             
-            context = {
+    context = {
                 'cancelaciones': cancelaciones_raw,
                 'stats_list': stats_list
             }
-
     return render(request, 'admin/cancelaciones/cancelaciones_admin.html', context)
 
 # VISTA PÚBLICA
