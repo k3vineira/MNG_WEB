1df08e8b0ab8c3c212458f4b049f308cd9c8bd40
daca-btl <dayanamozo2116@gmail.com>
2026-08-26 11:47:49 -0500

    fix: eliminar registro del modelo PaquetePromocion en admin.py

--- App/admin.py
+++ App/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Usuario, Temporada, Categoria, Actividades, Paquete, Tarifa, PaqueteActividad, Blog, PQRS, Seguimiento, Reserva, Auditoria, Calificacion, PlanGuia, Pago, Factura, Promocion, PaquetePromocion,  Poliza, SeguroViaje
+from .models import Usuario, Temporada, Categoria, Actividades, Paquete, Tarifa, PaqueteActividad, Blog, PQRS, Seguimiento, Reserva, Auditoria, Calificacion, PlanGuia, Pago, Factura, Promocion,  Poliza, SeguroViaje
 
 # Registros simples de modelos de la aplicación en el panel de administración
 admin.site.register(Usuario)
@@ -19,6 +19,5 @@ admin.site.register(PlanGuia)
 admin.site.register(Pago)
 admin.site.register(Factura)
 admin.site.register(Promocion)
-admin.site.register(PaquetePromocion)
 admin.site.register(Poliza)
 admin.site.register(SeguroViaje)
