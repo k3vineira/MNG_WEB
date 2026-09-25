b1ca0ee5132072b1cbb97dccac2d3fae2dff2e37
Kevin Neira <neirak424@gmail.com>
2026-09-24 22:29:50 -0500

    feat: añadir el icono en todos los apartados de el proyecto

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static as static_url
+
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static_url('img/monagua_log.ico'), permanent=True)),
     path('', include('App.urls')),
     path('admin/', admin.site.urls),
     path('autenticacion/', include('autenticacion.urls')),
